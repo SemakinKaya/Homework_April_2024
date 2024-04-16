1faf58f86d13889b4d12f6a4d3cbc01e57e431d4
SemakinKaya <semakinkay@gmail.com>
2024-04-16 17:38:00 +0200

    add April:Homework2

---
+++ April/Homework2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Add сoordinate X: ");
+Console.WriteLine("Add сoordinate Y: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("First Coordinate Plane");
+}
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("Second Coordinate Plane");
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("Third Coordinate Plane");
+}
+if (x == 0 && y == 0)
+{
+    Console.WriteLine("Error");
+}
