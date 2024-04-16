fa84f3b3a559416372e6854b010e71738607095a
SemakinKaya <semakinkay@gmail.com>
2024-04-16 15:13:06 +0200

    add April:Homework1

---
+++ April/Homework1/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Add digit: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+int number1 = 7;
+int number2 = 23;
+
+if (digit % number1 == 0 && digit % number2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
