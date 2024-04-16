429ed78dce40cb3e9e3b03c4776a96af9c09094f
SemakinKaya <semakinkay@gmail.com>
2024-04-16 18:52:26 +0200

    add April:Homework3

---
+++ April/Homework3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход 
+//целое число из отрезка 
+// [10, 99] и показывает наибольшую цифру числа.
+
+System.Console.WriteLine("Enter a number from the segment [10, 99]: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 10 && number <= 99)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    if (firstDigit >= secondDigit)
+    {
+        System.Console.WriteLine(firstDigit);
+    }
+    else
+    {
+        System.Console.WriteLine(secondDigit);
+    }
+}
+else
+{
+    System.Console.WriteLine("you added the wrong number according to the range");
+}
